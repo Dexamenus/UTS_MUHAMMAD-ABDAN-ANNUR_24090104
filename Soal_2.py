185f316e0be1e4129e518f2e61879c8ddadbf85c
Dexamenus <abdan.annur123@gmail.com>
2024-11-13 11:49:50 +0700

    Add files via upload

--- Soal_2.py
+++ Soal_2.py
@@ -2,7 +2,7 @@ thn = int(input('Masukan Tahun : '))
 
 if thn % 400 == 0:
     hasil = (f"Tahun {thn} merupakan TAHUN KABISAT")
-elif thn % 4 == 0 :
+elif thn % 4 == 0 and thn % 100 != 0:
     hasil = (f"Tahun {thn} merupakan TAHUN KABISAT")
 else:
     hasil = ("Bukan Tahun Kabisat")
